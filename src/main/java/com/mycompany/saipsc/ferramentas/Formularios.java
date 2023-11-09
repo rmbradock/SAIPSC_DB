eb1204329998422b49d5b166660c46b806de1483
RmBradock <rmbradock@hotmail.com>
2023-11-09 11:27:15 -0300

    alualizacao0911

---
+++ src/main/java/com/mycompany/saipsc/ferramentas/Formularios.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.saipsc.ferramentas;
+
+import javax.swing.JFrame;
+import javax.swing.JDialog;
+
+/**
+ *
+ * @author Batista
+ */
+public class Formularios {
+    
+    public static JFrame cadAcesso;
+    public static JFrame listAcesso;
+    
+    public static JFrame cadCampeonato;
+    public static JFrame listCampeonato;
+    
+    public static JFrame cadCargo;
+    public static JFrame listCargo;
+    
+    public static JFrame cadCategoria;
+    public static JFrame listCategoria;
+    
+    public static JFrame cadCondicao;
+    public static JFrame listCondicao;
+    
+    public static JFrame cadCor;
+    public static JFrame listCor;
+    
+    public static JFrame cadMarca;
+    public static JFrame listMarca;
+    
+    public static JFrame cadPatrimonio;
+    public static JFrame listPatrimonio;
+    
+    public static JFrame cadPessoa;
+    public static JFrame listPessoa;
+    
+    public static JFrame cadPolos;
+    public static JFrame listPolos;
+    
+    public static JFrame cadTamanho;
+    public static JFrame listTamanho;
+}
